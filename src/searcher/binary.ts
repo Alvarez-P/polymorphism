d0e441394ceed4b10a9f58b4f4939a1f76e706c7
alvarez98 <le17081478@merida.tecnm.mx>
2021-02-28 22:17:09 -0600

    Change binary search

--- src/searcher/binary.ts
+++ src/searcher/binary.ts
@@ -1,21 +1,18 @@
 import ISearcher from 'searcher/searcher.interface'
 
 class BinarySearcher implements ISearcher {
-  constructor(private numbers: number[]) {}
-
-  sort() {
-    return this.numbers.sort((a, b) => a - b)
+  constructor(private numbers: number[]) {
+    this.numbers = this.numbers.sort((a, b) => a - b)
   }
-  
+
   search(number): number {
-    const sortedArray = this.sort()
     let low = 0,
-      high = sortedArray.length - 1,
+      high = this.numbers.length - 1,
       mid: number
     while (low <= high) {
       mid = Math.floor((low + high) / 2)
-      if (sortedArray[mid] === number) return mid
-      else if (sortedArray[mid] < number) low = mid + 1
+      if (this.numbers[mid] === number) return mid
+      else if (this.numbers[mid] < number) low = mid + 1
       else high = mid - 1
     }
     return -1
